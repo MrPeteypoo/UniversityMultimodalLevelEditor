1dbe351d8a58573a9c96600f351bad8961c2e88f
Simon Peter Campbell <storm20200@gmail.com>
2014-12-10 18:51:15 +0000

    Added more to the IItem class and implemented the weapon class.

--- Source/WaterTokenLevelEditor/Source/IItem.cs
+++ Source/WaterTokenLevelEditor/Source/IItem.cs
@@ -10,7 +10,7 @@ namespace WaterTokenLevelEditor
     /// <summary>
     /// Defines the type of item, useful for knowing what type to cast down to without run-time type checking.
     /// </summary>
-    public sealed enum ItemType
+    public enum ItemType
     {
         Weapon  = 0,
         Supply  = 1
@@ -18,19 +18,67 @@ namespace WaterTokenLevelEditor
 
 
     /// <summary>
-    /// An interface to every Item class in the game, this is the interface used by weapons, supplies and accessories.
+    /// An abtract base class for every Item class in the game, this is the interface used by weapons, supplies and accessories.
     /// </summary>
-    public interface IItem
+    public abstract class IItem
     {
+        #region Implementation data
+
         protected string    m_name      = "";   //!< The name of the item as it should be displayed in-game.
         protected uint      m_uses      = 1;    //!< How many times the item can be used by characters.
         protected uint      m_worth     = 2;    //!< How much the item is worth, therefore how much it can be bought and sold for.
         protected uint      m_weight    = 0;    //!< How quickly the item can be used by characters, effects turn order.
 
+        #endregion
+
+
+        #region Getters, setters and properties
 
         /// <summary>
         /// Gets type of item the object is, useful for knowing how to cast.
         /// </summary>
-        public abstract ItemType itemType;
+        public abstract ItemType GetItemType();
+
+
+        /// <summary>
+        /// Gets or sets the name of the Item, this is displayed in-game.
+        /// </summary>
+        public string name
+        {
+            get { return m_name; }
+            set { m_name = value; }
+        }
+
+
+        /// <summary>
+        /// Gets or sets how many times the item can be used, this will always be more than 0.
+        /// </summary>
+        public uint uses
+        {
+            get { return m_uses; }
+            set { m_uses = Math.Max (1, value); }
+        }
+
+
+        /// <summary>
+        /// Gets or sets how much the item can be bought for, this effects the selling value and will always be more than 1.
+        /// </summary>
+        public uint worth
+        {
+            get { return m_worth; }
+            set { m_worth = Math.Max (2, value); }
+        }
+
+
+        /// <summary>
+        /// Gets or sets the weight of the item. This effects how quickly characters can use it in-game.
+        /// </summary>
+        public uint weight
+        {
+            get { return m_weight; }
+            set { m_weight = value; }
+        }
+
+        #endregion
     }
 }
---
+++ Source/WaterTokenLevelEditor/Source/Weapon.cs
@@ -0,0 +1,232 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace WaterTokenLevelEditor
+{
+    /// <summary>
+    /// The type of each weapon in the game, required by the game so that only cerain characters can use weapons they're able to.
+    /// </summary>
+    public enum WeaponType
+    {
+        Sword   = 0,
+        Axe     = 1, 
+        Lance   = 2,
+        Bow     = 3,
+        Fire    = 4,
+        Thunder = 5,
+        Wind    = 6,
+        Light   = 7,
+        Staff   = 8,
+        Knife   = 9
+    }
+
+
+    /// <summary>
+    /// An enumerator of each available weapon rank in the game. Determines whether characters have enough skill to weild the weapon or not.
+    /// </summary>
+    public enum WeaponRank
+    {
+        Null    = 0,
+        S       = 1,
+        A       = 2,
+        B       = 3,
+        C       = 4,
+        D       = 5,
+        E       = 6
+    }
+
+
+    /// <summary>
+    /// The data representation of a Weapon item, these are used by characters in the game to cause damage.
+    /// </summary>
+    public sealed class Weapon : IItem
+    {
+        #region Implementation data
+
+        private WeaponType  m_weaponType    = WeaponType.Sword;         //!< Indicates what type the weapon is, and therefore, who can use it.
+        private WeaponRank  m_rank          = WeaponRank.E;             //!< The skill level required to actually use the weapon.
+        
+        private uint        m_might         = 0;                        //!< How strong the weapon is, adds to the base damage.
+        
+        private double      m_hitRate       = 1.0;                      //!< The hit rate in percentage of the weapon.
+        private double      m_criticalRate  = 0.0;                      //!< How likely the weapon is to critically hit another character.
+
+        private Range<uint> m_range         = new Range<uint> (1,1);    //!< The range at which the weapon can hit characters.
+
+        private uint        m_experience    = 1;                        //!< How much experience a character gains on their weapon rank from using the weapon.
+
+        #endregion
+
+
+        #region Constructors and operators
+
+        /// <summary>
+        /// The default constructor for the Weapon class.
+        /// </summary>
+        public Weapon() { }
+
+
+        /// <summary>
+        /// The default copy constructor for the Weapon class.
+        /// </summary>
+        /// <param name="weapon">The object to copy data from.</param>
+        public Weapon (Weapon weapon)
+        {
+            if (weapon)
+            {
+                m_weaponType = weapon.m_weaponType;
+                m_rank = weapon.m_rank;
+
+                m_might = weapon.m_might;
+
+                m_hitRate = weapon.m_hitRate;
+                m_criticalRate = weapon.m_criticalRate;
+
+                m_range = new Range<uint> (weapon.m_range);
+
+                m_experience = weapon.m_experience;
+            }
+
+            else
+            {
+                throw new ArgumentNullException ("Attempt to initialise a Weapon object using the copy constructor with a null pointer.");
+            }
+        }
+
+
+        /// <summary>
+        /// A simple operator overload for the "if (variable) ;" shorthand.
+        /// </summary>
+        /// <param name="range">The object to check.</param>
+        /// <returns>Whether the object is a null pointer.</returns>
+        public static implicit operator bool (Weapon weapon)
+        {
+            return weapon != null;
+        }
+
+        #endregion
+
+
+        #region Getters, setters and properties
+
+        /// <summary>
+        /// Gets type of item the object is, useful for knowing how to cast.
+        /// </summary>
+        public override ItemType GetItemType()
+        {
+            return ItemType.Weapon;
+        }
+
+        /// <summary>
+        /// Gets or sets the weapon type of the weapon.
+        /// </summary>
+        public WeaponType weaponType
+        {
+            get { return m_weaponType; }
+            set { m_weaponType = value; }
+        }
+
+
+        /// <summary>
+        /// Gets or sets the rank of the weapon. WeaponRank.Null values will be silently ignored.
+        /// </summary>
+        public WeaponRank rank
+        {
+            get { return m_rank; }
+            set 
+            { 
+                if (value != WeaponRank.Null)
+                {
+                    m_rank = value;
+                }
+            }
+        }
+
+
+        /// <summary>
+        /// Gets or sets the might value of the weapon, this effects how powerful the weapon is.
+        /// </summary>
+        public uint might
+        {
+            get { return m_might; }
+            set { m_might = value; }
+        }
+
+
+        /// <summary>
+        /// Gets or sets the hit rate value of the weapon, this should be in percentage where 1.0 == 100%. Values below 0.0 will be clamped.
+        /// </summary>
+        public double hitRate
+        {
+            get { return m_hitRate; }
+            set { m_hitRate = Math.Max (value, 0.0); }
+        }
+
+
+        /// <summary>
+        /// Gets or sets the critical hit rate of the weapon, this will be clamped between 0.0 and 1.0.
+        /// </summary>
+        public double criticalRate
+        {
+            get { return m_criticalRate; }
+            set
+            {
+                if (value < 0.0)
+                {
+                    m_criticalRate = 0.0;
+                }
+
+                else if (value > 1.0)
+                {
+                    m_criticalRate = 1.0;
+                }
+
+                else
+                {
+                    m_criticalRate = value;
+                }
+            }
+        }
+
+
+        /// <summary>
+        /// Gets or sets the range of the weapon. The range cannot have a minimum or maximum of 0.
+        /// </summary>
+        public Range<uint> range
+        {
+            get { return m_range; }
+            set
+            {
+                if (value)
+                {
+                    if (value.minimum != 0 && value.maximum != 0)
+                    {
+                        m_range = value;
+                    }
+                }
+
+                else
+                {
+                    throw new ArgumentNullException ("Attempt to set Weapon.range to null.");
+                }
+            }
+        }
+    
+
+        /// <summary>
+        /// Gets or sets the experience value of the weapon, this effects the rate at which a characters weapon rank increases.
+        /// </summary>
+        public uint experience
+        {
+            get { return m_experience; }
+            set { m_experience = value; }
+        }
+
+        #endregion
+
+    }
+}
