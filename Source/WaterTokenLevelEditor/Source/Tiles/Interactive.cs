94ec2884b4ddd039ef9ba1517d1969e70387cbd1
Simon Peter Campbell <storm20200@gmail.com>
2014-12-11 20:19:59 +0000

    Implmented the interactive tile classes.

---
+++ Source/WaterTokenLevelEditor/Source/Tiles/Interactive.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace WaterTokenLevelEditor
+{
+    /// <summary>
+    /// An interface to every interactive layer class in the game.
+    /// </summary>
+    public abstract class Interactive : TileLayer
+    {
+        /// <summary>
+        /// Obtains the enumeration of the type for this class.
+        /// </summary>
+        /// <returns>LayerType.Interactive.</returns>
+        public sealed override LayerType GetLayerType()
+        {
+            return LayerType.Interactive;
+        }
+
+
+        /// <summary>
+        /// Obtains the enumeration for the particular sub-class of the interactive layer.
+        /// </summary>
+        /// <returns>The child-specific enumeration.</returns>
+        public abstract InteractiveType GetInteractiveType();
+    }
+}
