2fc0c44ddbb31a0923d2c370ac11f62fc93d4f91
Simon Peter Campbell <storm20200@gmail.com>
2014-12-19 06:51:53 +0000

    Character stats updating works.

--- Source/WaterTokenLevelEditor/MainWindow.xaml.cs
+++ Source/WaterTokenLevelEditor/MainWindow.xaml.cs
@@ -24,13 +24,16 @@ namespace WaterTokenLevelEditor
     {
         #region Implementation data
         
-        private LevelGrid   m_grid              = null;             //!< The management class for the displayable grid.
-        
-        private Control     m_selectedControl   = null;             //!< The currently selected UI control.
-        private int         m_selectedTile      = 0;               //!< The currently selected grid tile.
-        //private LayerType   m_selectedCategory  = LayerType.Null;   //!< The category of the selected control.
+        private LevelGrid   m_grid              = null;                 //!< The management class for the displayable grid.
 
-        private bool        m_unsavedChanges    = false;            //!< Prompts the user to save when they risk losing data.
+        private string      m_workingDirectory  = "";                   //!< Used for setting the sprite location of tiles.
+        
+        private Control     m_selectedControl   = null;                 //!< The currently selected UI control.
+        private int         m_selectedTile      = 0;                    //!< The currently selected grid tile.
+
+        private ContextMenu m_tileMenu          = null;                 //!< The context menu used when tiles are right-clicked.
+
+        private bool        m_unsavedChanges    = false;                //!< Prompts the user to save when they risk losing data.
 
         #endregion
 
@@ -50,6 +53,8 @@ namespace WaterTokenLevelEditor
 
             // Force the zoom level to update.
             sdr_zoom.Value = sdr_zoom.Value;
+
+            m_workingDirectory = System.AppDomain.CurrentDomain.BaseDirectory;
 
             lbl_statusLabel.Content = "Application ready...";
         }
@@ -112,6 +117,20 @@ namespace WaterTokenLevelEditor
             return null;
         }
 
+
+        /// <summary>
+        /// Gets the context menu ready for usage.
+        /// </summary>
+        private void SetupContextMenu()
+        {
+            ContextMenu menu = new ContextMenu();
+
+            // Create the menu items.
+            MenuItem reset = new MenuItem () { Header = "_Reset to default", Tag = 0 };
+            MenuItem interactive = new MenuItem () { Header = "_Remove interactive object", Tag = 1 };
+            MenuItem character = new MenuItem () { Header = "_Remove character object", Tag = 2  };
+        }
+
         #endregion
 
 
@@ -132,11 +151,13 @@ namespace WaterTokenLevelEditor
                     case MessageBoxResult.Yes:
                         //TODO: Save
                         // Just pass in the returned value in case the user decides to cancel their action.
+                        m_unsavedChanges = false;
                         CreateNewLevel (ProcessGridSize());
                         break;
                     
                     case MessageBoxResult.No:
                         // Just pass in the returned value in case the user decides to cancel their action.
+                        m_unsavedChanges = false;
                         CreateNewLevel (ProcessGridSize());
                         break;
 
@@ -288,7 +309,7 @@ namespace WaterTokenLevelEditor
             else
             {
                 m_selectedControl = null;
-            }            
+            }
         }
 
         private void Image_MouseLeftButtonDown (object sender, MouseButtonEventArgs e)
@@ -305,7 +326,126 @@ namespace WaterTokenLevelEditor
         #endregion
                 
         
-        #region Properties panel
+        #region Character properties
+        
+        /// <summary>
+        /// This nasty function creates a character tile from all the information currently available and assigns it to the selected tile.
+        /// </summary>
+        private void UpdateCharacterLayer()
+        {
+            if (IsInitialized)
+            {
+                try
+                {
+                    // Obtain the character.
+                    Character character = m_grid.GetGameTile (m_selectedTile).character;
+
+                    UpdateCharacterCore (character);
+                    UpdateCharacterStats (character);
+                    UpdateCharacterWeaponRanks (character);
+                }
+
+                catch (Exception error)
+                {
+                    lbl_statusLabel.Content = "Error updating character tile: " + error.Message;
+                }
+            }
+        }
+
+
+        /// <summary>
+        /// Simply updates the core character information from the properties panel.
+        /// </summary>
+        /// <param name="character">The character object to modify.</param>
+        private void UpdateCharacterCore (Character character)
+        {
+            character.name              = txt_charName.Text.Trim();
+            character.characterType     = (CharacterType) cmb_charType.SelectedIndex;
+            character.characterClass    = (Class) cmb_charClass.SelectedIndex;
+            character.affinity          = (Element) cmb_charAffinity.SelectedIndex;
+            character.level             = (uint) sdr_charLevel.Value;
+            
+        }
+
+
+        /// <summary>
+        /// Simply updates the stats information from the properties panel.
+        /// </summary>
+        /// <param name="character">The character object to modify.</param>
+        private void UpdateCharacterStats (Character character)
+        {
+            // Obtain and modify the stats values.
+            Stats stats         = character.stats;
+                    
+            stats.hp            = (uint) sdr_charHP.Value;
+            stats.strength      = (uint) sdr_charSTR.Value;
+            stats.magic         = (uint) sdr_charMAG.Value;
+            stats.skill         = (uint) sdr_charSKL.Value;
+            stats.speed         = (uint) sdr_charSPD.Value;
+            stats.luck          = (uint) sdr_charLCK.Value;
+            stats.defense       = (uint) sdr_charDEF.Value;
+            stats.resistance    = (uint) sdr_charRES.Value;
+            stats.constitution  = (uint) sdr_charCON.Value;
+            stats.weight        = (uint) sdr_charWEI.Value;
+            stats.movement      = (uint) sdr_charMOV.Value;
+            
+        }
+
+
+        /// <summary>
+        /// Simply updates the weapon rank information from the properties panel.
+        /// </summary>
+        /// <param name="character">The character object to modify.</param>
+        private void UpdateCharacterWeaponRanks (Character character)
+        {
+            // Obtain and modify the weapon rank values.
+            WeaponRanks ranks   = character.ranks;
+                
+            ranks.sword     = (WeaponRank) cmb_charRankSword.SelectedIndex;
+            ranks.axe       = (WeaponRank) cmb_charRankAxe.SelectedIndex;
+            ranks.lance     = (WeaponRank) cmb_charRankLance.SelectedIndex;
+            ranks.bow       = (WeaponRank) cmb_charRankBow.SelectedIndex;
+            ranks.fire      = (WeaponRank) cmb_charRankFire.SelectedIndex;
+            ranks.thunder   = (WeaponRank) cmb_charRankThunder.SelectedIndex;
+            ranks.wind      = (WeaponRank) cmb_charRankWind.SelectedIndex;
+            ranks.light     = (WeaponRank) cmb_charRankLight.SelectedIndex;
+            ranks.staff     = (WeaponRank) cmb_charRankStaff.SelectedIndex;
+            ranks.knife     = (WeaponRank) cmb_charRankKnife.SelectedIndex;
+        }
+
+
+        /// <summary>
+        /// This should simply request the characters values to be updated.
+        /// </summary>
+        private void ComboBox_UpdateChar (object sender, SelectionChangedEventArgs e)
+        {
+            UpdateCharacterLayer();
+        }
+
+
+        /// <summary>
+        /// This is called whenever any character related TextBox is updated.
+        /// </summary>
+        private void TextBox_UpdateChar (object sender, TextChangedEventArgs e)
+        {
+            UpdateCharacterLayer();
+        }
+
+
+        /// <summary>
+        /// This is called whenever any character related Slider is updated.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Slider_UpdateChar (object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            UpdateCharacterLayer();
+        }
+
+        #endregion
+
+
+        #region Misc events
 
         /// <summary>
         /// Manages the displaying of sprites in the level grid as well as setting sprite variable of each tile.
@@ -318,23 +458,26 @@ namespace WaterTokenLevelEditor
                 TextBox textBox = sender as TextBox;
 
                 // Ensure text is valid.
-                string text = textBox.Text.Trim();                
+                string text = textBox.Text.Trim();
                 textBox.Text = text;
 
                 // Obtain the required data.
                 LayerType layer = (LayerType) Convert.ToInt32 (textBox.Tag);
+
                 Image sprite = m_grid.GetImageLayer (m_selectedTile, layer);
                 TileLayer data = m_grid.GetGameTile (m_selectedTile).GetLayer (layer);
 
+                string location = m_workingDirectory + text;
+
                 // Attempt to set the correct values.
-                if (File.Exists (System.AppDomain.CurrentDomain.BaseDirectory + text))
+                if (File.Exists (location))
                 {                   
-                    sprite.Source = new BitmapImage (new Uri (@text, UriKind.RelativeOrAbsolute));
+                    sprite.Source = new BitmapImage (new Uri (@location, UriKind.RelativeOrAbsolute));
                     
                     if (data)
                     {
                         data.sprite = text;
-                    }                    
+                    }
                 }
 
                 else
@@ -352,12 +495,10 @@ namespace WaterTokenLevelEditor
             {
                 lbl_statusLabel.Content = "Unable to load sprite: " + error.Message;
             }
+
+            m_unsavedChanges = true;
         }
 
-        #endregion
-
-
-        #region Misc events
 
         /// <summary>
         /// Handles the zooming functionality of the level grid.
@@ -372,8 +513,5 @@ namespace WaterTokenLevelEditor
         }
 
         #endregion
-
-
-
     }
 }
